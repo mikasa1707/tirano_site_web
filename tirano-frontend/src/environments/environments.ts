458992e72eebc4e6954160d0b76d26b706b076c0
mikasa1707 <raveroson73@gmail.com>
2026-08-17 10:46:18 +0300

    0.1.1

--- tirano-frontend/src/environments/environments.ts
+++ tirano-frontend/src/environments/environments.ts
@@ -2,5 +2,5 @@ export const environment = {
   production: false,
 
   // apiUrl: 'http://localhost:3000',
-  apiUrl: 'http://192.168.88.17:3000',
+  apiUrl: 'https://tirano-site-web.onrender.com:3000',
 };
